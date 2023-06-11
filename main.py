e50ee612186e96b9046bf37cb10fb5ed013b9f95
Ron Hernaus <ron.hernaus@rws.nl>
2023-06-11 12:37:12 +0200

    add multiprocessing

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
+from multiprocessing import Pool
+
+
+def worker(number):
+    steps = multiplicative_persistence(number)
+    return (steps, number)
+
 def multiplicative_persistence(number):
-    steps = 1
-    total = 1
-    for i in str(number):
-        total = total * int(i)
-    # print(f"{steps} - {total}")
-    while len(str(total)) > 1:
+    steps = 0
+    while number > 9:
         steps += 1
-        number = total
-        total = 1
-        for i in str(number):
-            total = total * int(i)
-        # print(f"{steps} - {total}")
+        product = 1
+        while number:
+            product *= number % 10
+            number //= 10
+            if product == 0:
+                break
+        number = product
     return steps
 
-max_steps = 0
-max_number = 0
-for i in range(1, 9999999999999999):
-    steps = multiplicative_persistence(i)
-    if steps > max_steps:
-        max_steps = steps
-        max_number = i
-        print(f"{max_steps} - {max_number}")
+if __name__ == '__main__':
+    max_steps = 0
+    max_number = 0
+    with Pool() as pool:
+        for steps, number in pool.imap_unordered(worker, range(1, 9999999999999999), chunksize = 100000):
+            if steps > max_steps:
+                max_steps = steps
+                max_number = number
+                print(f"{max_steps} - {max_number}")
